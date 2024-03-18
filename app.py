ffe20c76bd98bf651a1f8527aeb983189befd80b
Brian Ellis Sound <brian.e2014@gmail.com>
2024-03-18 19:13:23 -0400

    init

---
+++ app.py
@@ -0,0 +1,31 @@
+import time
+import os
+from flask import Flask, request
+
+UPLOAD_FOLDER = './uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        newFileName = time.time()+".png"
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        file1.save(path)
+        return path
+
+        return 'ok'
+    return '''
+    <h1>Upload new File</h1>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="file1">
+      <input type="submit">
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run()
---
+++ go.py
@@ -0,0 +1,63 @@
+import numpy as np
+from PIL import Image
+from scipy.io.wavfile import write
+from scipy.io.wavfile import read
+import os
+
+shapeImage = (1024,1024,3)
+shapeAudio = (3145728,)
+singleLenSize = 1024*1024*3
+
+def doAndSay(com):
+	print(com)
+	os.system(com)
+
+def imgToAud(imgName, audName):
+	print(imgName)
+	imgNamePrefix = imgName.split(".")[0]
+	exactName = "exact_"+imgNamePrefix+".png"
+	doAndSay("convert "+imgName+"  -resize 1024x1024\!  "+exactName)
+	
+	img = Image.open(exactName).convert('RGB')
+	# convert it to a matrix
+	
+	vector = np.asarray(img).reshape(shapeAudio)
+	print(vector.dtype)
+	print(vector)
+	# do something to the vector
+	print(vector.shape)
+
+	newv = vector
+	samplerate = 44100;
+
+	biggestNumb = np.iinfo(np.int16).max
+	newnewv = ((newv / 255.0) * (biggestNumb*2)) - biggestNumb
+	write(audName, samplerate, newnewv.astype(np.int16))
+
+def audToImage(audName, imgName):
+	print(audName)
+	audNamePrefix = audName.split(".")[0]
+	exactName = "exact_"+audNamePrefix+".wav"
+	doAndSay("sox "+audName+" "+exactName+" remix 1")
+
+	sr, dat = read(exactName)
+	print(dat)
+	if(dat.size > singleLenSize):
+		dat = dat[:singleLenSize]
+	if(dat.size<singleLenSize):
+		print(singleLenSize-dat.size)
+		dat = np.pad(dat, (0, singleLenSize-dat.size), 'constant')
+	print(dat.size)
+	print(singleLenSize)
+	# dat = (((dat / np.iinfo(np.int16).max) + 1) * (255.0 *10 /2)).round().astype(np.uint8) # np.uint8 vs np.int16 // keeps skin tone same while distoring other colors
+	# dat = (((dat / np.iinfo(np.int16).max) + 1) * (255.0)).round().astype(np.int16) # classic distortion / grainy
+	dat = (((dat / np.iinfo(np.int16).max) + 1) * (255.0/2)).round().astype(np.uint8) # TRUE 1:1 MAPPING
+	print(dat)
+
+	arr4 = np.asarray(dat).reshape(shapeImage)
+	img2 = Image.fromarray(arr4, 'RGB')
+	img2.save(imgName)
+
+# imgToAud("t.png","test1.wav")
+audToImage("test2.wav","test0.png")
+
