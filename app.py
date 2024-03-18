22ea4d7126f153bcdfd84b547c22c2f56ad1e083
Brian Ellis Sound <brian.e2014@gmail.com>
2024-03-18 19:37:00 -0400

    yep

--- app.py
+++ app.py
@@ -2,30 +2,24 @@ import time
 import os
 from flask import Flask, request
 
-UPLOAD_FOLDER = './uploads'
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app)
 
-@app.route('/', methods=['POST'])
+UPLOAD_IMG_FOLDER = './uploadedImages'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_IMG_FOLDER
+
+@app.route('/uploadIMG', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        if 'file1' not in request.files:
-            return 'there is no file1 in form!'
-        file1 = request.files['file1']
-        newFileName = time.time()+".png"
-        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
-        file1.save(path)
-        return path
-
-        return 'ok'
-    return '''
-    <h1>Upload new File</h1>
-    <form method="post" enctype="multipart/form-data">
-      <input type="file" name="file1">
-      <input type="submit">
-    </form>
-    '''
+    if 'file1' not in request.files:
+        return 'there is no file1 in form!'
+    file1 = request.files['file1']
+    newFileName = time.time()+".png"
+    path = os.path.join(UPLOAD_IMG_FOLDER, file1.filename)
+    file1.save(path)
+    return path
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3007, debug=True)
